e11636e84c176806dc01fc37d2f2987dd47252a3
Alexandra S <alexandrastanjic@gmail.com>
2020-10-06 23:36:30 +0300

    mobile menu js code done

--- resources/main.js
+++ resources/main.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+
+const btnIconOpen = document.getElementById('btnIconOpen');
+const btnIconClose = document.getElementById('btnIconClose');
+const mobileMenu = document.getElementById('mobileMenu');
+
+btnIconOpen.addEventListener('click', function() {
+    mobileMenu.classList.remove('hidden'); 
+});
+
+btnIconClose.addEventListener('click', function() {
+  mobileMenu.classList.add('hidden'); 
+});
