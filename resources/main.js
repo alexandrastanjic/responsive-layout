0bddc1651ce5fabe0f63be4901c559b42f154b8a
Alexandra S <alexandrastanjic@gmail.com>
2020-10-19 16:32:02 +0300

    modified css file for mobile version

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,8 @@
 
 const btnIconOpen = document.getElementById('btnIconOpen');
 const btnIconClose = document.getElementById('btnIconClose');
+const mobileMenu = document.getElementById('mobileMenu');
+
 
 btnIconOpen.addEventListener('click', function() {
     mobileMenu.classList.remove('hidden'); 
